69b397ab2cf02eaa9f6bae43ab325ce940200ade
rahul1907935 <83805686+rahul1907935@users.noreply.github.com>
2022-01-11 19:22:56 +0530

    POSITIVE NUMBER
    This file has been printed successfully

---
+++ posneg.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 19:11:30 2022
+
+@author: HP
+"""
+
+list1 = [12, -7, 5, 64, -14]
+
+
+for num in list1:
+   if num >= 0 :
+    
+    print(num, end = " ")
+
+list2 = [12, 14, -95, 3]    
+   
+
+for num in list2:
+       if num >= 0 :
+        
+        print(num, end = " ")
